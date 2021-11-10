1540fd0f5e873e5a682e7f9b2c02ab6c0a7c0a0a
nhuse <nathan.huse@yahoo.com>
2021-11-09 18:24:11 -0600

    Added auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
@@ -12,4 +13,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
